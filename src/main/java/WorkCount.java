636e04ca3464a52f8321219f76ef3a1390ae4549
sun <15566743435@163.com>
2018-08-28 11:28:37 +0800

    init preject

---
+++ src/main/java/WorkCount.java
@@ -0,0 +1,92 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+/**
+ * <p>
+ * 类名称：WorkCount
+ * </p>
+ * <p>
+ * 类描述：${DESCRIPTION}
+ * </p>
+ * <p>
+ * 创建人：sun
+ * </p>
+ * <p>
+ * 创建时间：2018-08-28 9:29
+ * </p>
+ * <p>
+ * 修改人：
+ * </p>
+ * <p>
+ * 修改时间：
+ * </p>
+ * <p>
+ * 修改备注：
+ * </p>
+ * <p>
+ * Copyright (c) 版权所有
+ * </p>
+ *
+ * @version 1.0.0
+ */
+public class WorkCount {
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        IntWritable intWritable = new IntWritable(1);
+        Text text = new Text();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String valueByStr = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(valueByStr);
+            while (tokenizer.hasMoreTokens()) {
+                text.set(tokenizer.nextToken());
+                context.write(text, intWritable);
+            }
+        }
+    }
+
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            Iterator<IntWritable> iterator = values.iterator();
+            while (iterator.hasNext()) {
+                sum += iterator.next().get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setCombinerClass(Reduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setJarByClass(WorkCount.class);
+        FileInputFormat.addInputPath(job, new Path("C:\\software\\LocalPreject\\mapreduce\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\software\\LocalPreject\\mapreduce\\output"));
+
+        boolean b = job.waitForCompletion(true);
+        System.out.println(b);
+
+    }
+
+}
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,68 @@
+import java.lang
+import java.util.StringTokenizer
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
+
+/**
+  *
+  * <p>
+  * 类名称：WordCOunt
+  * </p>
+  * <p>
+  * 类描述：${DESCRIPTION}
+  * </p>
+  * <p>
+  * 创建人：sun
+  * </p>
+  * <p>
+  * 创建时间：2018-08-28 10:09
+  * </p>
+  * <p>
+  * 修改人：
+  * </p>
+  * <p>
+  * 修改时间：
+  * </p>
+  * <p>
+  * 修改备注：
+  * </p>
+  * <p>
+  * Copyright (c) 版权所有
+  * </p>
+  *
+  * @version 1.0.0
+  *
+  */
+object WordCount {
+
+  import scala.collection.JavaConversions._
+
+  def main(args: Array[String]): Unit = {
+    val mapper: Mapper[LongWritable, Text, Text, IntWritable] = new Mapper[LongWritable, Text, Text, IntWritable]() {
+      private val intWritable = new IntWritable(1)
+      override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, IntWritable]#Context): Unit = value.toString.split(" ").foreach(str => context.write(new Text(str), intWritable))
+    }
+
+    val reducer: Reducer[Text, IntWritable, Text, IntWritable] = new Reducer[Text, IntWritable, Text, IntWritable]() {
+      override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = context.write(key, new IntWritable(values.iterator().map(_.get()).sum))
+    }
+    val configuration = new Configuration()
+    val job = new Job(configuration)
+    job.setMapperClass(mapper.getClass)
+    job.setReducerClass(reducer.getClass)
+    job.setJarByClass(WordCount.getClass)
+    job.setOutputKeyClass(classOf[Text])
+    job.setOutputValueClass(classOf[IntWritable])
+    FileInputFormat.addInputPath(job, new Path("C:\\software\\LocalPreject\\mapreduce\\input"))
+    FileOutputFormat.setOutputPath(job, new Path("C:\\software\\LocalPreject\\mapreduce\\output"))
+    val bool = job.waitForCompletion(true)
+    println(bool)
+
+  }
+
+}
